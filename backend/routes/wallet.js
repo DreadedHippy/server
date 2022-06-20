e3f747c5d706c9d279173c919e9a05ed0674e070
DreadedHippy <onotaizee@gmail.com>
2022-06-20 13:06:21 +0100

    Prevent Duplicate Wallets

--- backend/routes/wallet.js
+++ backend/routes/wallet.js
@@ -1,34 +1,43 @@
 const { result } = require('lodash');
 const wallet = require('../models/wallet');
 const Wallet = require('../models/wallet')
-const User = require('../models/user')
+const User = require('../models/user');
+const user = require('../models/user');
+const { response } = require('express');
 
 
 exports.create = function(req, res, next) {
-  User.findOne({email: req.body.email})
-  .then(user => {
-    const wallet = new Wallet({
-      name: req.body.name,
-      address: req.body.address,
-      currency: req.body.currency,
-      iconSrc: req.body.iconSrc,
-      balance: req.body.balance,
-      transactions: [],  
-    })
-    if (!user){
-      return res.status(401).json({
-        message: 'Internal email mismatch problem'
-      });
-    };
-    if (!user.wallets){
-      user.wallets =[]
-    }
-    user.wallets.push(wallet)
-    user.save()
-    console.log(user)
-    return res.status(200).json({
-      message: 'Wallet Added',
-      wallet: wallet
-    })
+  const wallet = new Wallet({
+    name: req.body.name,
+    address: req.body.address,
+    currency: req.body.currency,
+    iconSrc: req.body.iconSrc,
+    balance: req.body.balance,
+    transactions: [],  
   })
+  User.updateOne(
+    {email: req.body.email, 'wallets.address': {$ne: wallet.address}},
+    {$push: {wallets: wallet}}
+  )
+  .then(result => {
+    console.log(result)
+    if(result.modifiedCount == 0){
+      return res.status(400).json({
+        message: 'Wallet Aaready added',
+        result: result
+      })
+    }
+    if(result.modifiedCount !== 0){
+      return res.status(200).json({
+        message: 'Wallet Saved',
+        wallet: result
+      })
+    }
+  }).catch(err =>{
+    console.log(err)
+    return res.status(500).json({
+      err: err
+    })  
+  })
+
 }
